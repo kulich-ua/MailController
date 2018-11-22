b5c7205812f6e02dbdfca01463c131fbb2a5b73e
Volodymyr Kolibaba <kulich.ua@gmail.com>
2018-11-22 17:06:10 +0100

    create Example project

---
+++ Example/MailControllerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MailControllerExample
+//
+//  Created by Volodymyr Kolibaba on 22.11.18.
+//  Copyright © 2018 Volodymyr Kolibaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
