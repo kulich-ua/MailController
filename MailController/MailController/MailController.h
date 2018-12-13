42cb019bf006c7a5c93aef8d3c5f6abc5ed7a893
Volodymyr Kolibaba <kulich.ua@gmail.com>
2018-12-13 15:45:38 +0100

    create Cocoa Touch Framework project

---
+++ MailController/MailController/MailController.h
@@ -0,0 +1,19 @@
+//
+//  MailController.h
+//  MailController
+//
+//  Created by Volodymyr Kolibaba on 13.12.18.
+//  Copyright © 2018 Volodymyr Kolibaba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MailController.
+FOUNDATION_EXPORT double MailControllerVersionNumber;
+
+//! Project version string for MailController.
+FOUNDATION_EXPORT const unsigned char MailControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MailController/PublicHeader.h>
+
+
