2b961c0d869e1fe6667a7378c2d58d4cc14af756
Volodymyr Kolibaba <kulich.ua@gmail.com>
2018-12-17 15:15:41 +0100

    create unit tests for MailController using MockMailComposeViewController

--- Tests/MailControllerTests.swift
+++ Tests/MailControllerTests.swift
@@ -9,26 +9,34 @@
 import XCTest
 @testable import MailController
 
+import MessageUI
+
+
+struct MockMailComposeViewController: MailComposeViewController {
+
+    var mailComposeDelegate: MFMailComposeViewControllerDelegate?
+    
+    static func canSendMail() -> Bool {
+        
+        return true
+    }
+    
+    func dismiss(animated flag: Bool, completion: (() -> Void)?) {
+        
+    }
+}
+
 class MailControllerTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testWithMFMailComposeViewController() {
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        let mailComposeViewController = MailController.shared.mailComposeViewController()
+        XCTAssertNil(mailComposeViewController)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testWithMock() {
+        
+        let mailComposeViewController = MailController.shared.mailComposeViewController(type: MockMailComposeViewController.self)
+        XCTAssertNotNil(mailComposeViewController)        
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
